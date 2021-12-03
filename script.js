18d4992d0da444aaf20d4e0b158586558114198e
yousifr <yousifr.ismail@gmail.com>
2021-12-03 17:54:16 +0300

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexcolors = "#";
+  for (let i = 0; i < 6; i++) {
+    hexcolors += hex[randomColors()];
+  }
+  color.textContent = hexcolors;
+  document.body.style.backgroundColor = hexcolors;
+});
+function randomColors() {
+  return Math.floor(Math.random() * hex.length);
+}
